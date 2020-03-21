b3f3855adbe9ebd1b92c55e85aa21d43cce66c5f
Moritz Makowski <makowskimoritz@gmail.com>
2020-03-21 14:11:42 +0100

    Add API endpoint functions

---
+++ flask_backend/resources/api_authentication.py
@@ -0,0 +1,142 @@
+from flask_backend import db, bcrypt
+from flask_backend.models.db_account import DBAccount
+from flask_backend.models.db_authentication import APIKey
+from flask_backend import BCRYPT_SALT
+
+import random
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Helper Functions
+
+def validate_email_format(email):
+    if email is None:
+        return False
+    else:
+        l1 = email.split("@")
+        if len(l1) == 2:
+            l2 = l1[1].split(".")
+            if len(l2) == 2:
+                return True
+        return False
+
+
+def generate_random_key(length=32):
+    possible_characters = []
+
+    # Characters '0' through '9'
+    possible_characters += [chr(x) for x in range(48, 58)]
+
+    # Characters 'A' through 'Z'
+    possible_characters += [chr(x) for x in range(65, 91)]
+
+    # Characters 'a' through 'z'
+    possible_characters += [chr(x) for x in range(97, 123)]
+
+    random_key = ""
+
+    for i in range(length):
+        random_key += random.choice(possible_characters)
+
+    return random_key
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Functions to be accessed from 'routes' directly
+
+def login_account(email, password):
+    if validate_email_format(email) and len(password) >= 8:
+        account = DBAccount.query.filter(DBAccount.email == email).first()
+
+        if account is not None:
+            if bcrypt.check_password_hash(account.password, password + BCRYPT_SALT):
+                api_key = register_client(account)
+                return {"status": "ok",
+                        "email": email,
+                        "api_key": api_key}
+        else:
+            return {"status": "invalid email/password"}
+    else:
+        return {"status": "invalid email/password"}
+
+
+
+def is_authenticated(email, api_key, new_api_key=False):
+    if validate_email_format(email) and len(api_key) >= 8:
+        account = DBAccount.query.filter(DBAccount.email == email).first()
+
+        if account is not None:
+            api_key_object = APIKey.query\
+                .filter(APIKey.account_id == account.id)\
+                .filter(APIKey.key == api_key).first()
+        else:
+            return {"status": "invalid request"}
+
+        if api_key_object is None:
+            return {"status": "invalid request"}
+
+        if new_api_key:
+            # A new api_key is generated every time the user does this
+            unregister_client(account)
+            new_api_key = register_client(account)
+        else:
+            new_api_key = api_key
+
+        return {"status": "ok",
+                "email": email,
+                "api_key": new_api_key}
+    else:
+        return {"status": "invalid email/api key"}
+
+
+def logout_account(email, api_key):
+    # Why do you need both email and api_key?
+    # So that someone cannot just logout random
+    # emails or api_keys to randomly hit something
+    # You only know both if you have regularly
+    # logged in
+    account = DBAccount.query.filter(DBAccount.email == email).first()
+
+    if account is None:
+        return False
+
+    unregister_client(account)
+
+    return True
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Direct database manipulation
+
+def register_client(account):
+    # Kick out any old key where the user did not log himself out
+    APIKey.query.filter(APIKey.account_id == account.id).delete()
+
+    api_key = APIKey()
+    api_key.account_id = account.id
+
+    # Generating a new random API key
+    api_key.key = generate_random_key()
+
+    db.session.add(api_key)
+    db.session.commit()
+    return api_key.key
+
+
+def unregister_client(account):
+    # Kick out any old key
+    APIKey.query.filter(APIKey.account_id == account.id).delete()
+    db.session.commit()
+    return True
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Just small tests
+
+if __name__ == "__main__":
+    print(generate_random_key(length=16))
+    print(generate_random_key())
+    print(generate_random_key(length=48))
+    print(generate_random_key(length=64))
---
+++ flask_backend/resources/rest_account.py
@@ -0,0 +1,220 @@
+from flask_restful import Resource
+from flask_backend.models.db_authentication import DBAccount, APIKey
+from flask_backend.routes import get_params_dict
+
+from flask import request
+import requests
+import json
+
+from flask_backend.resources import api_authentication
+from flask_backend import db, bcrypt
+from flask_backend import BCRYPT_SALT, GCP_API_KEY
+
+
+class RESTAccount(Resource):
+    # IMPORTANT: For all of the following operations (except creating accounts) a
+    # valid email/api_key pair must be provided! Otherwise private data might
+    # leak to non-authorized entities
+
+    def get(self):
+        # Get all infos for a specific account
+        params_dict = get_params_dict(request)
+
+        if api_authentication.is_authenticated(params_dict["email"], params_dict["api_key"], new_api_key=False)["status"] == "ok":
+            account = DBAccount.query.filter(DBAccount.email == params_dict["email"]).first()
+        else:
+            return {"status": "invalid request"}
+
+        if account is None:
+            return {"status": "invalid request"}
+        else:
+            return {
+                "status": "ok",
+                "account": {
+                    "name": account.name,
+                    "contact": {
+                        "email": account.email,
+                        "phone": account.phone,
+                    },
+                    "address": {
+                        "street": account.street,
+                        "zip": account.zip,
+                        "city": account.city,
+                        "country": account.country,
+                    },
+                    "coordinates": {
+                        "lat": account.lat,
+                        "lng": account.lng,
+                    }
+                }
+            }
+
+
+    def post(self):
+        # Create a new account
+        params_dict = get_params_dict(request)
+
+        new_account = DBAccount()
+
+        if "account_name" in params_dict:
+            new_account.name = params_dict["account_name"]
+        else:
+            return {"status": "name is missing"}
+
+        if "account_email" in params_dict:
+            new_account.email = params_dict["account_email"]
+            new_account.email_confirmed = False
+        else:
+            return {"status": "email is missing"}
+
+        if "account_phone" in params_dict:
+            new_account.phone = params_dict["account_phone"]
+            new_account.phone_confirmed = False
+        else:
+            return {"status": "phone is missing"}
+
+        if "account_password" in params_dict:
+            password = params_dict["account_password"]
+            hashed_password = bcrypt.generate_password_hash(password + BCRYPT_SALT).decode('UTF-8')
+            new_account.password = hashed_password
+        else:
+            return {"status": "password is missing"}
+
+        if "account_street" in params_dict:
+            new_account.street = params_dict["account_street"]
+        else:
+            return {"status": "street is missing"}
+
+        if "account_zip" in params_dict:
+            new_account.zip = params_dict["account_zip"]
+        else:
+            return {"status": "zip is missing"}
+
+        if "account_city" in params_dict:
+            new_account.city = params_dict["account_city"]
+        else:
+            return {"status": "city is missing"}
+
+        if "account_country" in params_dict:
+            new_account.country = params_dict["account_country"]
+        else:
+            return {"status": "country is missing"}
+
+        request_url = f"https://maps.googleapis.com/maps/api/geocode/json?" \
+                      f"address={new_account.street.replace(' ', '+')},{new_account.zip}+{new_account.city},{new_account.country}&" \
+                      f"key={GCP_API_KEY}"
+
+        try:
+            response = requests.get(request_url)
+        except:
+            return {"status": "invalid google geocode request"}
+
+        try:
+            coordinates_dict = json.loads(response.text)["results"][0]["geometry"]["location"]
+            new_account.lat = coordinates_dict["lat"]
+            new_account.lng = coordinates_dict["lng"]
+        except KeyError as e:
+            db.session.expire(new_account)
+            return {"status": "invalid google geocode response"}
+
+        db.session.add(new_account)
+        db.session.commit()
+
+        # log in account and return email/api_key
+        login_result_dict = api_authentication.login_account(params_dict["account_email"], params_dict["account_password"])
+        return login_result_dict
+
+
+    def put(self):
+        # Modify an existing account
+        params_dict = get_params_dict(request)
+
+        if api_authentication.is_authenticated(params_dict["email"], params_dict["api_key"], new_api_key=False)["status"] == "ok":
+            account = DBAccount.query.filter(DBAccount.email == params_dict["email"]).first()
+        else:
+            return {"status": "invalid request"}
+
+        if account is None:
+            return {"status": "invalid request"}
+        else:
+
+            if "account_name" in params_dict:
+                account.name = params_dict["account_name"]
+
+            if "email" in params_dict:
+                if not account.email_confirmed:
+                    account.email = params_dict["email"]
+                    account.email = params_dict["email"]
+                else:
+                    return {"status": "email has already been confirmed"}
+
+            if "account_phone" in params_dict:
+                if not account.phone_confirmed:
+                    account.phone = params_dict["account_phone"]
+                else:
+                    return {"status": "phone has already been confirmed"}
+
+            if "account_old_password" in params_dict and "account_new_password" in params_dict:
+                old_password = params_dict["account_old_password"]
+                new_password = params_dict["account_new_password"]
+                if bcrypt.check_password_hash(account.password, old_password + BCRYPT_SALT):
+                    hashed_password = bcrypt.generate_password_hash(new_password + BCRYPT_SALT).decode('UTF-8')
+                    account.password = hashed_password
+                    account.password = hashed_password
+                else:
+                    db.session.expire(account)
+                    return {"status": "password invalid"}
+
+            if "account_street" in params_dict:
+                account.street = params_dict["account_street"]
+
+            if "account_zip" in params_dict:
+                account.zip = params_dict["account_zip"]
+
+
+            request_url = f"https://maps.googleapis.com/maps/api/geocode/json?" \
+                          f"address={account.street.replace(' ', '+')},{account.zip}+{account.city},{account.country}&" \
+                          f"key={GCP_API_KEY}"
+
+            try:
+                response = requests.get(request_url)
+            except:
+                return {"status": "invalid google geocode request"}
+
+            try:
+                coordinates_dict = json.loads(response.text)["results"][0]["geometry"]["location"]
+                account.lat = coordinates_dict["lat"]
+                account.lng = coordinates_dict["lng"]
+            except KeyError as e:
+                db.session.expire(account)
+                return {"status": "invalid google geocode response"}
+
+            db.session.commit()
+
+            return {"status": "ok"}
+
+    def delete(self):
+        # Delete an existing account
+        params_dict = get_params_dict(request)
+
+        if api_authentication.is_authenticated(params_dict["email"], params_dict["api_key"], new_api_key=False)["status"] == "ok":
+            account = DBAccount.query.filter(DBAccount.email == params_dict["email"]).first()
+        else:
+            return {"status": "invalid request"}
+
+        if account is None:
+            return {"status": "invalid request"}
+        else:
+
+            # Remove all related API Keys (logins)
+            account_id = DBAccount.query.filter(DBAccount.email == params_dict["email"]).first().id
+            APIKey.query.filter(APIKey.account_id == account_id).delete()
+
+            DBAccount.query.filter(DBAccount.email == params_dict["email"]).delete()
+
+            db.session.commit()
+
+            return {"status": "ok"}
+
+
+
